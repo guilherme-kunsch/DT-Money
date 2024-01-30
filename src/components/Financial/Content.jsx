2cb8b5929e6dbf66ee5e9d8669b8afd8e2cba8fc
Guilherme Kunsch <suporte@iconexlog.com.br>
2024-01-30 16:56:14 -0300

    alterando modal

--- src/components/Financial/Content.jsx
+++ src/components/Financial/Content.jsx
@@ -4,9 +4,9 @@ import styles from './Content.module.css';
 export function Content() {
   return (
     <div className={styles.cardFinanceiro}>
-      <Card name="Entrada" value={1000}/>
-      <Card name="Saida" value={1500}/>
-      <Card name="Total" value={500} />
+      <Card name="Entrada" value={0}/>
+      <Card name="Saida" value={0}/>
+      <Card name="Total" value={0} />
     </div>
   )
 }
